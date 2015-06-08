8b30c5442d53846d130285015ae1616b00c12b70
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2015-06-08 23:53:43 +0530

    commit1

---
+++ corpus.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+import sys, os, getopt, codecs, re
+
+usage = "\n\nNAME\n\n\n\tcorpus - process corpus\n\n\nSYNOPSIS\n\n\n\t./corpus.py [OPTIONS]... <FILE|DIR>...\n\n\nDESCRIPTION\n\n\n\tProcess corpus. Transliterate if specified. If from the FIRE corpus, strip <TAGS> and extract text </TAGS>. Print to stdout unless write file specified.\n\n\tMandatory arguments to long options are mandatory for short options too.\n\n\nOPTIONS\n\n\t-h, --help\n\t\tdisplay this help and exit\n\n\t-v, --verbose\n\t\tdisplay information for debugging\n\n\t-f, --fire\n\t\tassume FILE|DIR in FIRE corpus document format\n\n\t-w, --write=FILE\n\t\taccept file for writing\n\n\t-t, --transliterate=FILE\n\t\taccept file for transliteration\n\n\nARGUMENTS\n\n\tFILE\n\t\taccept file for preprocessing\n\n\tDIR\n\t\taccept directory for preprocessing\n\n\nAUTHOR\n\n\n\tWritten by Amitrajit Sarkar.\n\n\nREPORTING BUGS\n\n\n\tReport bugs to <aaiijmrtt@gmail.com>.\n\n\nCOPYRIGHT\n\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Amitrajit Sarkar\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\n"
+
+tags = re.compile("^<DOC>\n<DOCNO>[^<]*</DOCNO>\n<TEXT>([^<]*)</TEXT>\n</DOC>$")
+glob = re.compile("([^\Z]*)")
+tran = re.compile("^([^\t]*)\t([^\n]*)\n")
+verbose = False
+fire = False
+
+def error(description):
+	print description
+	sys.exit()
+
+def arguments(string):
+
+	global verbose, fire
+	namein = None
+	nameout = None
+	dirnamein = None
+	transname = None
+
+	try:
+		opts, args = getopt.getopt(string, "hvfw:t:", ["help", "verbose", "fire", "write=", "transliterate="])
+	except getopt.GetoptError:
+		error(usage)
+
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			error(usage)
+		elif opt in ("-v", "--verbose"):
+			verbose = True
+		elif opt in ("-f", "--fire"):
+			fire = True
+		elif opt in ("-w", "--write"):
+			nameout = arg
+		elif opt in ("-t", "--transliterate"):
+			transname = arg
+
+	if not args:
+		error(usage)
+	if os.path.isdir(args[0]):
+		dirnamein = args[0]
+	else:
+		namein = args[0]
+	if verbose:
+		print "[DEBUG: PARSED COMMANDLINE ARGUMENT read: %s ]\n[DEBUG: PARSED COMMANDLINE ARGUMENT write: %s ]\n[DEBUG: PARSED COMMANDLINE ARGUMENT directory: %s ]\n[DEBUG: PARSED COMMANDLINE ARGUMENT transliterate: %s ]" %(namein, nameout, dirnamein, transname)
+
+	return namein, nameout, dirnamein, transname
+
+def translit(filetrans):
+	trans = dict()
+	with codecs.open("trans.txt", "r", "utf-8") as transfile:
+		for line in transfile.readlines():
+			match = tran.match(line)
+			if match is None:
+				error("[EXIT: DICTIONARY FORMAT ERROR]")
+			else:
+				trans[match.group(1)] = match.group(2)
+	if verbose:
+		print "[DEBUG: TRANSLITERATED BY file: %s ]" %filetrans
+	return trans
+
+def readwrite(filein, fileout, trans = None):
+	text = filein.read()
+	match = tags.match(text) if fire else glob.match(text)
+	if match is None:
+		error("[EXIT: DOCUMENT FORMAT ERROR]")
+	if trans is None:
+		fileout.write(match.group(1))
+	else:
+		for tokenin in match.group(1):
+			tokenout = trans[tokenin] if tokenin in trans else tokenin
+			fileout.write(tokenout)
+
+if __name__ == '__main__':
+
+	namein, nameout, dirnamein, transname = arguments(sys.argv[1:])
+	fileout = sys.stdout if nameout is None else codecs.open(nameout, "w", "utf-8")
+	trans = translit(transname) if transname is not None else None
+
+	if dirnamein is None:
+		if namein is None:
+			error(usage)
+		with codecs.open(namein, "r", "utf-8") as filein:
+			readwrite(filein, fileout, trans)
+			if verbose:
+				print "[DEBUG: PREPROCESSED file: %s ]" %namein
+	else:
+		for root, dirs, files in os.walk(dirnamein, topdown = False):
+			for name in files:
+				with codecs.open(os.path.join(root, name), "r", "utf-8") as filein:
+					readwrite(filein, fileout, trans)
+					if verbose:
+						print "[DEBUG: PREPROCESSED file: %s ]" %name
+
+	if fileout is not sys.stdout:
+		fileout.close()
---
+++ vectors.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+import sys, getopt, re, numpy
+
+usage = "\n\nNAME\n\n\n\tvectors - process monolingual word vectors and bilingual dictionary\n\n\nSYNOPSIS\n\n\n\t./vectors.py [OPTIONS]... <FILE:SRCVEC> <FILE:TGTVEC> <FILE:BIDIC>...\n\n\nDESCRIPTION\n\n\n\tProcess monolingual word vectors and bilingual dictionary. Generate translation matrix. Print to stdout unless write file specified.\n\n\tMandatory arguments to long options are mandatory for short options too.\n\n\nOPTIONS\n\n\t-h, --help\n\t\tdisplay this help and exit\n\n\t-v, --verbose\n\t\tdisplay information for debugging\n\n\t-w, --write=FILE\n\t\taccept file for writing\n\n\t-t, --translator=FILE\n\t\taccept file containing translator matrix\n\n\nARGUMENTS\n\n\tFILE:SRCVEC\n\t\taccept file containing source language word vectors\n\n\tFILE:TGTVEC\n\t\taccept file containing target language word vectors\n\n\tFILE:BIDIC\n\t\taccept file containing dictionary from source to target language\n\n\nAUTHOR\n\n\n\tWritten by Amitrajit Sarkar.\n\n\nREPORTING BUGS\n\n\n\tReport bugs to <aaiijmrtt@gmail.com>.\n\n\nCOPYRIGHT\n\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Amitrajit Sarkar\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\n"
+
+head = re.compile("^([^ ]*) ([^\n]*)(\n)")
+entry = re.compile("^([^ ]*)( )")
+tran = re.compile("^([^\t]*)\t([^\n]*)\n")
+floatsize = 4
+verbose = False
+
+def error(description):
+	print description
+	sys.exit()
+
+def arguments(string):
+
+	global verbose
+	nameout = None
+	sourcevectors = None
+	targetvectors = None
+	transferwords = None
+	translator = None
+
+	try:
+		opts, args = getopt.getopt(string, "hvw:t:", ["help", "verbose", "write=", "translator="])
+	except getopt.GetoptError:
+		error(usage)
+
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			error(usage)
+		elif opt in ("-v", "--verbose"):
+			verbose = True
+		elif opt in ("-w", "--write"):
+			nameout = arg
+		elif opt in ("-t", "--translator"):
+			translator = arg
+
+	if len(args) != 3:
+		error(usage)
+	else:
+		sourcevectors = args[0]
+		targetvectors = args[1]
+		transferwords = args[2]
+
+	if verbose:
+		print "[DEBUG: PARSED COMMANDLINE ARGUMENT write: %s ]\n[DEBUG: PARSED COMMANDLINE ARGUMENT translator: %s ]\n[DEBUG: PARSED COMMANDLINE ARGUMENT sourcevectors: %s ]\n[DEBUG: PARSED COMMANDLINE ARGUMENT targetvectors: %s ]\n[DEBUG: PARSED COMMANDLINE ARGUMENT dictionary: %s ]" %(nameout, translator, sourcevectors, targetvectors, transferwords)
+
+	return nameout, translator, sourcevectors, targetvectors, transferwords
+
+def dictionary(filename):
+
+	trans = dict()
+	with open(filename, "r") as transfile:
+		for line in transfile.readlines():
+			match = tran.match(line)
+			if match is None:
+				error("[EXIT: DICTIONARY FORMAT ERROR]")
+			else:
+				trans[match.group(1)] = match.group(2)
+	if verbose:
+		print "[DEBUG: TRANSLATION DICTIONARY INITIALIZED BY: %s]" %filename
+	return trans
+
+def vectorspace(filename):
+
+	space = dict()
+	with open(filename, "rb") as vecbin:
+		bytes = vecbin.read()
+		match = head.match(bytes)
+		if match is None:
+			error("[EXIT: BINARY FORMAT ERROR]")
+		vocabularysize = int(match.group(1))
+		embeddingsize = int(match.group(2))
+
+		sindex = match.start(3) + 1
+		for i in range(vocabularysize / 1000):
+			match = entry.match(bytes[sindex: ])
+			if match is None:
+				error("[EXIT: BINARY FORMAT ERROR]")
+			findex = match.start(2) + 1
+			space[match.group(1)] = numpy.fromstring(bytes[sindex + findex: ], dtype = numpy.float32, count = embeddingsize)
+			sindex += findex + embeddingsize * floatsize + 1
+	if verbose:
+		print "[DEBUG: VECTOR SPACE INITIALIZED BY: %s]" %filename
+	return space, embeddingsize
+
+def traintranslator(source, svectorsize, destination, tvectorsize, dictionary, translator = None, alpha = 0.05):
+	translator = numpy.random.rand(tvectorsize, svectorsize) if translator is None else translator
+	for word in dictionary:
+		if word in source and dictionary[word] in destination:
+			invector = source[word]
+			outvector = destination[dictionary[word]]
+			activation = numpy.dot(translator, invector)
+			difference = numpy.subtract(activation, outvector)
+			delta = numpy.dot(difference, invector.transpose())
+			delta = numpy.multiply(alpha, delta)
+			translator = numpy.subtract(translator, delta)
+	if verbose:
+		print "[DEBUG: TRANSLATION MATRIX GENERATED]"
+	return translator
+
+if __name__ == '__main__':
+
+	nameout, translatorfile, sourcevectors, targetvectors, transferwords = arguments(sys.argv[1: ])
+	source, svectorsize = vectorspace(sourcevectors)
+	target, tvectorsize = vectorspace(targetvectors)
+	dictionary = dictionary(transferwords)
+	translator = None if translatorfile is None else numpy.load(translatorfile, "r+")
+	translator = traintranslator(source, svectorsize, target, tvectorsize, dictionary, translator)
+	if nameout is None:
+		print translator
+	else:
+		numpy.save(nameout, translator)
+		if verbose:
+			print "[DEBUG: TRANSLATION MATRIX SAVED TO: %s.npy]" %nameout
